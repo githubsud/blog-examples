8fdb648ce1e26e8ed85a7ba59df7cef22e41916c
Moshe Bergman <mbergman@onekingslane.com>
2013-02-21 10:01:18 -0800

    initial app

---
+++ cache-helper/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CacheHelper::Application.config.secret_token = '03a6ef7af4d1eeaa3cdef296d627175cc3275bff83b0a301b205ebd5097d40a7686dc94765c1329146bbd2d3954156b7a4609cdbe02cece1fed128d9dbd07633'
